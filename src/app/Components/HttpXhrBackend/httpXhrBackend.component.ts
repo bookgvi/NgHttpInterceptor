4f45cff111ab33bb8c7c1455a4547c7ed362a7f3
book <bookgvi@gmail.com>
2020-06-01 01:10:12 +0300

    Custom(manual handler) for requests

--- src/app/Components/HttpXhrBackend/httpXhrBackend.component.ts
+++ src/app/Components/HttpXhrBackend/httpXhrBackend.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpEvent, HttpRequest, HttpResponse, HttpXhrBackend } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHandler, HttpRequest, HttpResponse, HttpXhrBackend } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { InterceptorsHandlerService } from '../../Services/Interceptors/interceptors.handler.service';
+import { RequestInterceptor } from '../../Services/Interceptors/request.interceptor';
 
 @Component({
   selector: 'app-http-xhr-backend',
@@ -18,7 +20,9 @@ export class HttpXhrBackendComponent implements OnInit {
   }
 
   ngOnInit() {
-    const req = new HttpRequest('GET', this.url); // Этот запрос проходит мимо стандартных интерсепторов
-    this.resp = this.backend.handle(req);
+    const req = new HttpRequest('GET', this.url);
+    const handler: HttpHandler = new InterceptorsHandlerService(this.backend, new RequestInterceptor());
+    this.resp = this.backend.handle(req); // Этот запрос проходит мимо стандартных интерсепторов
+    this.resp = handler.handle(req); // А в этот запрос вручную добавлен интерсептор
   }
 }
---
+++ src/app/Services/Interceptors/interceptors.handler.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class InterceptorsHandlerService implements HttpHandler {
+  constructor(private next: HttpHandler, private interceptor: HttpInterceptor) {
+  }
+
+  handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
+    return this.interceptor.intercept(req, this.next);
+  }
+
+}
